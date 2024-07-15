3433ded04c5ba3298b8a6fbaf31b610c4bb43749
samrudhiii <116385982+samrudhiii@users.noreply.github.com>
2024-07-15 18:48:56 +0530

    Create climbing_stairs.java

---
+++ climbing_stairs.java
@@ -0,0 +1,18 @@
+//Climbing_Stairs
+
+//Solution
+class Solution {
+    public int climbStairs(int n) {
+      int a = 0;
+      int b = 1;
+      int c = 0;
+
+      for(int i = 0;i<n;i++)
+      {
+          c=a+b;
+          a=b;
+          b=c;
+      }
+      return c;
+    }
+}
